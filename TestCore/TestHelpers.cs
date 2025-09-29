d62305d35d773e88ba95d2a6067f49a24051f537
Alicia <s1183889@student.windesheim.nl>
2025-09-29 21:03:33 +0200

    testscore update uitgewerkt
    In de testcore is de zelfde code als in sprint 3, dat ik hier in weer heb geplaatst.

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -31,14 +31,27 @@ namespace TestCore
         [Test]
         public void TestPasswordHelperReturnsFalse()
         {
-            Assert.Pass(); //Zelf uitwerken
+            string password = "usr3";
+            string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA=";
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
         }
 
         [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
         [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
         public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
         {
-            Assert.Fail(); //Zelf uitwerken zodat de test slaagt!
+            bool result = false;
+
+            try
+            {
+                result = PasswordHelper.VerifyPassword(password, passwordHash);
+            }
+            catch
+            {
+                result = false; 
+            }
+
+            Assert.IsFalse(result);        
         }
     }
 }
